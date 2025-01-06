5299cf4649e89bfa7929eecf332f69d3c2554a11
ekspla <ekspla@sky.plala.or.jp>
2025-01-06 12:41:10 +0900

    A minor clean-up.

--- nus_modem_server.py
+++ nus_modem_server.py
@@ -15,7 +15,6 @@ import asyncio
 import aioble
 import bluetooth
 from array import array
-import sys
 
 _NUS_SERVICE_UUID = bluetooth.UUID("6e400001-b5a3-f393-e0a9-e50e24dcca9e")
 _NUS_RX_CHARACTERISTIC_UUID = bluetooth.UUID("6e400002-b5a3-f393-e0a9-e50e24dcca9e")
